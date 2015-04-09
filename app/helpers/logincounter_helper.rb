aebee29a4df1cf1d3d1153d6437120d6340dea6e
Backspe <bschoe@gmail.com>
2015-04-09 15:24:42 +0900

    logincounter 컨트롤러 생성

---
+++ app/helpers/logincounter_helper.rb
@@ -0,0 +1,2 @@
+module LogincounterHelper
+end
---
+++ test/controllers/logincounter_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LogincounterControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
