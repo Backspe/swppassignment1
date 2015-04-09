f02095708459b473474283a7f405d38aaa64a4b7
Backspe <bschoe@gmail.com>
2015-04-09 17:09:11 +0900

    db migration

--- db/migrate/20150409063604_create_users.rb
+++ db/migrate/20150409063604_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :username
-      t.string, :password
-      t.int :count
+      t.string :username
+      t.string :password
+      t.integer :count
 
       t.timestamps null: false
     end
