4337f8df54944cec282d78f249eb8107b8c26155
Backspe <bschoe@gmail.com>
2015-04-09 20:04:29 +0900

    main view에 ajax를 사용하는 js 추가

---
+++ app/assets/javascripts/logincounter.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$('#login').click(function() {
+		var form_data = {
+			username: $('#username').val(),
+			password: $('#password').val()
+		};
+		$.ajax({
+			type: 'post',
+			url: '/login',
+			dataType: 'json',
+			data: form_data,
+			success: function(response) {
+				if(response.erroe_code == -4) {
+					$('#login_message').html='Invalid username and password combination. Please try again. ';
+				} else {
+					//TODO
+				}
+			}
+		});
+	});
+	$('#add_user').click(function() {
+		var form_data = {
+			username: $('#username').val(),
+			password: $('#password').val()
+		};
+		$.ajax({
+			type: 'post',
+			url: '/signup',
+			dataType: 'json',
+			data: form_data,
+			success: function(response) {
+				if(response.erroe_code == -1) {
+					$('#login_message').html='The user name should be 5~20 characters long. Please try again.';
+				} else if(response.erroe_code == -2) {
+					$('#login_message').html='The password should be 8~20 characters long. Please try again.';
+				} else if(response.erroe_code == -3) {
+					$('#login_message').html='This user name already exists. Please try again.';
+				} else {
+					//TODO
+				}
+			}
+		});
+	});
+});
